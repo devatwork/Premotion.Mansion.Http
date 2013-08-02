f82cbafb1e9d38235f038d7fc9fd7e279d63b6aa
Bert Willems <b.willems@premotion.nl>
2013-08-02 11:04:04 +0200

    + assembly is CLSCompliant

--- Premotion.Mansion.Http/Properties/AssemblyInfo.cs
+++ Premotion.Mansion.Http/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -38,3 +39,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: CLSCompliant(true)]
