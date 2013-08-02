a6f252fc813bccbf14986d9d08561fcc6c394295
Bert Willems <b.willems@premotion.nl>
2013-08-02 18:23:02 +0200

    + use Rx subject class to handle

--- Premotion.Mansion.Http/Streaming/StreamingWebConnection.cs
+++ Premotion.Mansion.Http/Streaming/StreamingWebConnection.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
 using Premotion.Mansion.Http.Patterns;
@@ -12,56 +13,8 @@ namespace Premotion.Mansion.Http.Streaming
 	/// Handles the connection details for connection for a Twitter streaming API.
 	/// </summary>
 	/// <typeparam name="T">The type of object streamed from this connection.</typeparam>
-	public abstract class StreamingWebConnection<T> : DisposableBase, IObservable<T>
+	public abstract class StreamingWebConnection<T> : DisposableBase
 	{
-		#region Nested type: Unsubscriber
-		/// <summary>
-		/// Unsubscribes a <see cref="IObserver{T}"/> when disposed.
-		/// </summary>
-		private class Unsubscriber : DisposableBase
-		{
-			#region Constructors
-			/// <summary>
-			/// Constructs the unsubsriber.
-			/// </summary>
-			/// <param name="observers">Reference to the list holding the <see cref="IObserver{T}"/>s.</param>
-			/// <param name="observer">The <see cref="IObserver{T}"/> which to unsubscribe.</param>
-			public Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
-			{
-				// validate arguments
-				if (observers == null)
-					throw new ArgumentNullException("observers");
-				if (observer == null)
-					throw new ArgumentNullException("observer");
-
-				// set the values
-				this.observers = observers;
-				this.observer = observer;
-			}
-			#endregion
-			#region IDisposable Members
-			/// <summary>
-			/// Dispose resources. Override this method in derived classes. Unmanaged resources should always be released
-			/// when this method is called. Managed resources may only be disposed of if disposeManagedResources is true.
-			/// </summary>
-			/// <param name="disposeManagedResources">A value which indicates whether managed resources may be disposed of.</param>
-			protected override void DisposeResources(bool disposeManagedResources)
-			{
-				// check for unmanaged resource disposal
-				if (!disposeManagedResources)
-					return;
-
-				// remove the observer from the list
-				if (observer != null)
-					observers.Remove(observer);
-			}
-			#endregion
-			#region Private Fields
-			private readonly IObserver<T> observer;
-			private readonly List<IObserver<T>> observers;
-			#endregion
-		}
-		#endregion
 		#region Constructors
 		/// <summary>
 		/// Constructs a new <see cref="StreamingWebConnection{T}"/> using the given <paramref name="client"/>.
@@ -114,8 +67,7 @@ namespace Premotion.Mansion.Http.Streaming
 							var token = Read(textReader);
 
 							// emit next tweet to the observers
-							foreach (var observer in observers)
-								observer.OnNext(token);
+							subject.OnNext(token);
 						}
 					}
 				}
@@ -142,8 +94,7 @@ namespace Premotion.Mansion.Http.Streaming
 					return;
 
 				// notify all the observers an unhandled error has occurred
-				foreach (var observer in observers)
-					observer.OnError(exception);
+				subject.OnError(exception);
 			}
 		}
 		/// <summary>
@@ -168,27 +119,6 @@ namespace Premotion.Mansion.Http.Streaming
 		/// <returns>Returns true if the error is handled, otherwise false.</returns>
 		protected abstract bool TryHandle(Exception exception, Func<Task> reconnect);
 		#endregion
-		#region IObservable<T> Members
-		/// <summary>
-		/// Notifies the provider that an observer is to receive notifications.
-		/// </summary>
-		/// <returns>
-		/// A reference to an interface that allows observers to stop receiving notifications before the provider has finished sending them.
-		/// </returns>
-		/// <param name="observer">The object that is to receive notifications.</param>
-		public IDisposable Subscribe(IObserver<T> observer)
-		{
-			// validate arguments
-			if (observer == null)
-				throw new ArgumentNullException("observer");
-
-			// add the subscriber
-			observers.Add(observer);
-
-			// create a Unsubscriber
-			return new Unsubscriber(observers, observer);
-		}
-		#endregion
 		#region DisposableBase Members
 		/// <summary>
 		/// Dispose resources. Override this method in derived classes. Unmanaged resources should always be released
@@ -197,22 +127,30 @@ namespace Premotion.Mansion.Http.Streaming
 		/// <param name="disposeManagedResources">A value which indicates whether managed resources may be disposed of.</param>
 		protected override void DisposeResources(bool disposeManagedResources)
 		{
+			// check for unmanaged disposal
 			if (!disposeManagedResources)
 				return;
+
+			// unsubscribe all observers
+			subject.Dispose();
 
 			// close the connection, if open
 			client.CancelPendingRequests();
 			client.Dispose();
-
-			// unsubscribe all observers
-			foreach (var observer in observers)
-				observer.OnCompleted();
-			observers.Clear();
+		}
+		#endregion
+		#region Properties
+		/// <summary>
+		/// Gets an <see cref="IObservable{T}"/> of <typeparamref name="T"/>.
+		/// </summary>
+		public IObservable<T> Stream
+		{
+			get { return subject.AsObservable(); }
 		}
 		#endregion
 		#region Private Fields
 		private readonly HttpClient client;
-		private readonly List<IObserver<T>> observers = new List<IObserver<T>>();
+		private readonly Subject<T> subject = new Subject<T>();
 		#endregion
 	}
 }
